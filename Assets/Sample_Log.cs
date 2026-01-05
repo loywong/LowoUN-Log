72ee63b8a1d9d790364b74c1ef8c62eb282d49be
LoyWong <loywongcom@gmail.com>
2026-01-05 10:19:19 +0800

    Init commit

---
+++ Assets/Sample_Log.cs
@@ -0,0 +1,26 @@
+﻿//注意: 需要添加脚本编译宏 PROJECT_LOG
+
+using LowoUN.Util;
+using UnityEngine;
+
+public class Sample_Log : MonoBehaviour {
+	[SerializeField] bool isDebug;
+	void Start () {
+		Log.Init (isDebug);
+
+		Log.Warn("some warning log");
+		Log.Error("some error log");
+
+		Log.Blue("data","log data");
+		Log.Gray("asset","log asset");
+		Log.Gray("ui","log ui");
+		Log.Green("scene","log scene");
+		Log.Green("flow","log flow");
+		Log.Trace("util","log util");
+		Log.Orange("test","log test");
+
+		Log.Trace("map","log map");
+		Log.Trace("battle","log battle");
+		Log.Trace("nav","log nav");
+	}
+}
